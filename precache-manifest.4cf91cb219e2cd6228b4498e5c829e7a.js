d3b586c26f1b86d23d5ef80177782b90c9552e2e
Fajar AM <somabangsa@gmail.com>
2021-03-26 22:24:51 +0700

    initial commit

---
+++ precache-manifest.4cf91cb219e2cd6228b4498e5c829e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b1f8bb7f3a3eae0832649d7444a221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87727964f8bff5f50c2",
+    "url": "/static/css/main.55a4d64d.chunk.css"
+  },
+  {
+    "revision": "8485a51f17b5d9c0a48a",
+    "url": "/static/js/2.d16c223f.chunk.js"
+  },
+  {
+    "revision": "c50c771658c38c7c1fcfc5b118622f46",
+    "url": "/static/js/2.d16c223f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87727964f8bff5f50c2",
+    "url": "/static/js/main.b21b6be8.chunk.js"
+  },
+  {
+    "revision": "152f4db5b8b201cc6796",
+    "url": "/static/js/runtime-main.63d582c3.js"
+  }
+]);
